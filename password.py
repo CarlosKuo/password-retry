863895153817d0458db4ed18512a1c2417abd800
郭逸翔 <yihsiang@guoyixiangdeMac-mini.local>
2023-09-17 20:49:00 +0800

    Change to while > 0

--- password.py
+++ password.py
@@ -23,12 +23,12 @@
 password = 'a123456'
 i = 3
 while i > 0:
+	i = i - 1
 	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		print('登入成功')
 		break
 	else:
-		i = i - 1
 		if i == 0:
 			break
 		print('密碼錯誤!還有', i, '次機會')
