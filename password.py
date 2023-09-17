5ec1dacafb55fb50241cb903dd39f3bb93f82727
郭逸翔 <yihsiang@guoyixiangdeMac-mini.local>
2023-09-17 20:46:55 +0800

    Change to while > 0

--- password.py
+++ password.py
@@ -5,15 +5,31 @@
 # 不對的話，就印出 "密碼錯誤! 還有 __ 次機會"
 # 對的話，就印出 "登入成功!"
 
+# password = 'a123456'
+# i = 3 # 剩餘機會
+# while True:
+# 	pwd = input('請輸入密碼: ')
+# 	if pwd == password:
+# 		print('登入成功')
+# 		break # 逃出迴圈
+# 	else:
+# 		i = i - 1
+# 		print('密碼錯誤!還有', i, '次機會')
+# 		if i == 0:
+# 			break
+
+
+# 不寫 True 的條件式
 password = 'a123456'
-i = 3 # 剩餘機會
-while True:
+i = 3
+while i > 0:
 	pwd = input('請輸入密碼: ')
 	if pwd == password:
 		print('登入成功')
-		break # 逃出迴圈
+		break
 	else:
 		i = i - 1
-		print('密碼錯誤!還有', i, '次機會')
 		if i == 0:
 			break
+		print('密碼錯誤!還有', i, '次機會')
+
